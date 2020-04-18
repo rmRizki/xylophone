538f5bbce327c192bb0ee0d0c48fcc7dcbdb44e4
rizki <kikirizmau140175@gmail.com>
2020-04-18 16:05:02 +0700

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,43 @@
+import 'package:audioplayers/audio_cache.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(XylophoneApp());
 
 class XylophoneApp extends StatelessWidget {
+  Widget soundButton(Color buttonColor, int soundNumber) {
+    return Expanded(
+      child: FlatButton(
+        color: buttonColor,
+        onPressed: () {
+          final player = AudioCache();
+          player.play('note$soundNumber.wav');
+        },
+        child: Image.asset(
+          'assets/ice_bear.png',
+          height: 60.0,
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         body: SafeArea(
-          child: Container(),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              soundButton(Colors.red[200], 1),
+              soundButton(Colors.orange[200], 2),
+              soundButton(Colors.yellow[200], 3),
+              soundButton(Colors.green[200], 4),
+              soundButton(Colors.blue[200], 5),
+              soundButton(Colors.indigo[200], 6),
+              soundButton(Colors.purple[200], 7),
+            ],
+          ),
         ),
       ),
     );
